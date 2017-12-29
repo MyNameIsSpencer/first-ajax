412a187acb7cb2d183a2e366f22fcdf89b816202
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-12-28 19:09:50 -0500

    click is working

--- ajax.js
+++ ajax.js
@@ -1,16 +1,66 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // var button = document.getElementById( '')
+  var request_to_root = document.getElementById( 'request_to_root')
+
+  request_to_root.addEventListener( 'click', function() {
+
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'text'
+    });
 
 
-
-
-  $.ajax({
-    url: 'http://first-ajax-api.herokuapp.com/',
-    method: 'GET',
-    data: ,
-    dataType: 'text'
   });
 
 
+
+
+
+
+
+
+
+
 });
+
+
+
+
+// button.addEventListener( 'click', function() {
+//     console.log('Button is CLICKED');
+//
+//     console.log('BEFORE AJAX');
+//
+//     //AJAX REQUEST HERE
+//     var request = $.ajax( {
+//       url: 'https://monsters-api.herokuapp.com/monsters',
+//       method: 'GET',
+//       dataType: 'html',
+//
+//     });
+//
+//     // console.log('Logging out `request.responseText`');
+//     // console.log( request.responseText );
+//
+//     request.done( function( data ) {
+//       console.log( 'REQUEST SUCCESS');
+//       // console.log( data );
+//
+//       var myElement = document.createElement( 'div');
+//       myElement.innerHTML = data;
+//       document.body.append(myElement);
+//     }).fail(function( jqXHR, textStatus, errorThrown) {
+//       console.log ( 'REQUEST FAILED' );
+//       console.log ( textStatus, errorThrown );
+//
+//       var msg = document.createElement( 'p' );
+//       msg.innerText = 'Whoopsi!';
+//       document.body.append( msg );
+//
+//     }).always(function() {
+//       console.log('REQUES;T COMPLETE');
+//     });
+//
+//     console.log('AFTER AJAX');
+//   });
