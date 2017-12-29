58cc8dfb23bffa62241636eb99c5447fc2bf683b
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-12-28 21:06:03 -0500

    done step 8

--- ajax.js
+++ ajax.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var button1 = document.getElementById( 'button1'),
-      button3 = document.getElementById( 'button3' );
+  var button1 = document.getElementById('button1'),
+      button3 = document.getElementById('button3'),
+      button7 = document.getElementById('button7'),
+      button8 = document.getElementById('button8');
 
   button1.addEventListener( 'click', function() {
     $.ajax({
@@ -25,7 +27,44 @@ document.addEventListener("DOMContentLoaded", function() {
       var fail_message = 'Has anyone ever told you how incredibly patient you are? It is so nice to come across someone as considerate as you. Oh man, oh geez! It looks like something went wrong! You don`t mind waiting while we fix it do you?';
       step3456.append(fail_message);
     }).always(function () {
-      console.log('All I know is, it`s done');
+      console.log('One way or the other, it`s done');
+    });
+  });
+
+  button7.addEventListener( 'click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      dataType: 'text'
+    }).done(function (responseData) {
+      console.log(responseData);
+      var pingAddOn = responseData;
+      step7.append(pingAddOn);
+    }).fail(function () {
+      console.log('Has anyone ever told you how incredibly patient you are? It is so nice to come across someone as considerate as you. Oh man, oh geez! It looks like something went wrong! You don`t mind waiting while we fix it do you?');
+      var fail_message = 'Has anyone ever told you how incredibly patient you are? It is so nice to come across someone as considerate as you. Oh man, oh geez! It looks like something went wrong! You don`t mind waiting while we fix it do you?';
+      step7.append(fail_message);
+    }).always(function () {
+      console.log('One way or the other, it`s done');
+    });
+  });
+
+  button8.addEventListener( 'click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      data: {timezone: 'Asia/Taipei'},
+      dataType: 'text'
+    }).done(function (responseData) {
+      console.log(responseData);
+      var pingAddOn = responseData;
+      step8.append(pingAddOn);
+    }).fail(function () {
+      console.log('Has anyone ever told you how incredibly patient you are? It is so nice to come across someone as considerate as you. Oh man, oh geez! It looks like something went wrong! You don`t mind waiting while we fix it do you?');
+      var fail_message = 'Has anyone ever told you how incredibly patient you are? It is so nice to come across someone as considerate as you. Oh man, oh geez! It looks like something went wrong! You don`t mind waiting while we fix it do you?';
+      step8.append(fail_message);
+    }).always(function () {
+      console.log('One way or the other, it`s done');
     });
   });
 
@@ -36,46 +75,4 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-
 });
-
-
-
-
-// button.addEventListener( 'click', function() {
-//     console.log('Button is CLICKED');
-//
-//     console.log('BEFORE AJAX');
-//
-//     //AJAX REQUEST HERE
-//     var request = $.ajax( {
-//       url: 'https://monsters-api.herokuapp.com/monsters',
-//       method: 'GET',
-//       dataType: 'html',
-//
-//     });
-//
-//     // console.log('Logging out `request.responseText`');
-//     // console.log( request.responseText );
-//
-//     request.done( function( data ) {
-//       console.log( 'REQUEST SUCCESS');
-//       // console.log( data );
-//
-//       var myElement = document.createElement( 'div');
-//       myElement.innerHTML = data;
-//       document.body.append(myElement);
-//     }).fail(function( jqXHR, textStatus, errorThrown) {
-//       console.log ( 'REQUEST FAILED' );
-//       console.log ( textStatus, errorThrown );
-//
-//       var msg = document.createElement( 'p' );
-//       msg.innerText = 'Whoopsi!';
-//       document.body.append( msg );
-//
-//     }).always(function() {
-//       console.log('REQUES;T COMPLETE');
-//     });
-//
-//     console.log('AFTER AJAX');
-//   });
